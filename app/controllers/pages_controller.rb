160e575c492b80c959e4deee9c011079676b0261
shlomi fogel <mm12g1@gmail.com>
2024-03-06 12:27:45 +0100

    find buddy method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def find_buddies
+    interests = current_user.interests if current_user
+    @users = User.joins(:interests).where(interests: { id: interests }).where.not(id: current_user.id).distinct
+  end
 end
