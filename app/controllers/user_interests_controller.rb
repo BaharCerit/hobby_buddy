9a04b9a5b36411224f659f5548f951710b3665c9
Jackie Almeida <jaqueliine.al@gmail.com>
2024-03-06 17:12:13 +0100

    interests page

--- app/controllers/user_interests_controller.rb
+++ app/controllers/user_interests_controller.rb
@@ -1,11 +1,14 @@
 class UserInterestsController < ApplicationController
   def new
-  
-
+    @interests = Interest.all
+    @user_interest = UserInterest.new
   end
 
   def create
+    interests = params[:user_interest][:interest_id]
+    interests.each do |interest_id|
+      new_interest = UserInterest.new(interest_id:, user: current_user)
+      new_interest.save!
+    end
   end
-
-
 end
