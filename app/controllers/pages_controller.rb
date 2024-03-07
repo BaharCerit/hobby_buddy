1b323e2fbd443d3ba181db996763c115d14ba27e
shlomi fogel <mm12g1@gmail.com>
2024-03-07 11:21:31 +0100

    end to pages#find_buddies

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,10 @@ class PagesController < ApplicationController
   def find_buddies
     interests = current_user.interests if current_user
     @users = User.joins(:interests).where(interests: { id: interests }).where.not(id: current_user.id).distinct
+  end
+
   def profile
-    @user = current_user
-    @user_interests = UserInterest.where(user_id: current_user)
+  @user = current_user
+  @user_interests = UserInterest.where(user_id: current_user)
   end
 end
