14de148c39829ac03ea402211402f40cd1396f38
shlomi fogel <mm12g1@gmail.com>
2024-03-11 14:57:18 +0100

    fixed the chatrooms

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,11 +2,12 @@ class ChatroomsController < ApplicationController
 
   def index
     # @chatrooms = Chatroom.all
-    @chatrooms = current_user.matches.map(&:chatroom)
-    # @chatrooms = Chatroom.joins(match: [:first_user, :second_user])
-    #                  .where(matches: { status: true, first_user: current_user })
-    #                  .or(Chatroom.joins(match: [:first_user, :second_user])
-    #                               .where(matches: { status: true, second_user: current_user }))
+
+    @chatrooms = current_user.matches.where(status: "accepted").map(&:chatroom)
+    # @chatrooms = Chatroom.joins(match: [:first_user_id, :second_user_id])
+                    #  .where(matches: { status: true, first_user_id: current_user })
+                    #  .or(Chatroom.joins(match: [:first_user, :second_user])
+                    #               .where(matches: { status: true, second_user: current_user }))
 
   end
 
