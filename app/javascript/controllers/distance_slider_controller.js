9e183574898bcffa83bcf550762fb4e454c4756f
shlomi fogel <mm12g1@gmail.com>
2024-03-12 17:14:09 +0100

    we have a distance filter

--- app/javascript/controllers/distance_slider_controller.js
+++ app/javascript/controllers/distance_slider_controller.js
@@ -2,13 +2,35 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="distance-slider"
 export default class extends Controller {
+  static targets = ["distance"]
   connect() {
-    this.updateSelectedDistance()
+
+    console.log('hello')
+
   }
 
-
-  updateSelectedDistance() {
-    const selectedDistance = this.rangeInputTarget.value
-    this.selectedDistanceTarget.textContent = `${selectedDistance} km`
+  display(event) {
+    this.distanceTarget.value = `${event.target.value} km `;
+    // this.updateSelectedDistance(event.target.value);
+    // this.sendDistanceToServer(event.target.value);
   }
-}
+
+//   updateSelectedDistance(value) {
+//     this.distanceTarget.innerHTML = value;
+//     console.log(value)
+//   }
+
+//   sendDistanceToServer(distance) {
+//     fetch(`/find-buddies?distance=${distance}`, { method: 'GET' })
+//       .then(response => response.text())
+
+//       .then(html => {
+//         // Assuming you have a target to update with the new content
+//         const targetElement = document.querySelector('[data-distance-slider-target="result"]');
+//         console.log(typeof html)
+
+//         targetElement.innerHTML = html;
+//       })
+//       .catch(error => console.error('Error:', error));
+//   }
+ }
